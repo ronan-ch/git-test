71b150c7f12d3783ad29cb1996bdac7b78068127
ronan-ch <ronan.c@goodnotesapp.com>
2023-07-02 14:41:14 +0800

    Add find algorithm

---
+++ Script/find_alg.js
@@ -0,0 +1,11 @@
+
+function find(buffer, target) {
+    for(let i=0;i<buffer.length;i++){
+        if(buffer[i] === target)
+            return i
+    }
+
+}
+
+var number = find([1,3,2,5,7,6],6);
+console.log(number)
