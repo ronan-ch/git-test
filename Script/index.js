34afd6c570c311afe5507a2c8039c0ae6c9b423b
ronan-ch <ronan.c@goodnotesapp.com>
2023-07-02 14:23:16 +0800

    add max function

--- Script/index.js
+++ Script/index.js
@@ -1,2 +1,15 @@
 
+function max(buffer) {
+    var max = buffer[0]
+    for(var i=0;i<buffer.length;i++){
+        if(buffer[i]>max){
+            max = buffer[i]
+        }
+
+    }
+    return max
+}
+
 console.log("this is a test file")
+var max1 = max([1,2,3,4,7]);
+console.log(max1)
